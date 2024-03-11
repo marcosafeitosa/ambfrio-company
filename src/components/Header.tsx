e21bf830d39dfcc5375caed49c0176f838f493e1
Marcos Davi <alietoriando@gmail.com>
2024-03-10 21:20:14 -0300

    [Feature]: Page Ambfrio

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <div className="bg-white mt-3">
+        <header>
+          <div className="">
+            <img src="../src/assets/logo-company.svg" alt="logo-company" />
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
