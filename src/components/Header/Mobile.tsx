1b70a2fde07ee64ac90639fb935fb23cd1175b67
Marcos Davi <alietoriando@gmail.com>
2024-03-16 10:53:14 -0300

    [Feature]: Componentes e Estilizacao

--- src/components/Header/Mobile.tsx
+++ src/components/Header/Mobile.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import Lists from '../Listas/Lists'
 import '../../index.css'
 
--- src/components/Listas/Lists.tsx
+++ src/components/Listas/Lists.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import '../../index.css'
 
 type Props = {
-  // itemCur: string[];
   classDiv?: string,
   classUl?: string,
   iconBoolean: boolean
-  // links?: string[]
 };
 
 type ListsHeader = {
